51653d9757e6847c3a40ace395407e1afe6e4899
StyleHub0 <elimarvin184@gmail.com>
2026-09-20 19:31:59 -0500

    Update loader URL in StyleDuels.lua

--- StyleDuels.lua
+++ StyleDuels.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.luarmor.us/files/v4/loaders/01434d93e28867b354f76cba7cb0cad9.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.us/files/v4/loaders/822d5d5ab018810102994b2ab64bb139.lua"))()
